d51066eb97b0beef8cc230bbe008bca103835757
Singh001Shubham <shubhamsingh9933@gmail.com>
2025-12-27 19:50:44 +0530

    update react installed

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+
+
+const heading = React.createElement("h1",{},"This is Namaste React");
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(heading)
